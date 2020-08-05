0c70dd467fe861cb1aec64119bccd228f3e54425
dan <dan@cs4p.net>
2020-08-05 14:15:51 -0400

    Added SQLite DB to repo

--- opal/settings.py
+++ opal/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'opal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db4.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
