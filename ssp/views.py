5715e7cd29cf19afbaec52ac025e7e33eb91fa7d
dan <dan@cs4p.net>
2020-11-03 15:42:16 -0500

    Added edit form for System Control

--- ssp/views.py
+++ ssp/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
 from django.shortcuts import *
 from django.views import generic
+from django.forms import modelformset_factory, modelform_factory, Textarea
 
-from .models import system_control, system_security_plan, nist_control
+from .models import system_control, system_security_plan, nist_control, control_parameter, control_statement
 from .forms import SystemSecurityPlan
 
 
@@ -28,6 +29,43 @@ def ssp_edit(request, pk):
         form = SystemSecurityPlan(instance=ssp)
     return render(request, 'ssp/ssp_edit.html', {'form': form})
 
+
+def system_control_edit(request, pk):
+    sc = get_object_or_404(system_control, pk=pk)
+    sc_FormSet = modelformset_factory(system_control,fields=('control_status','control_origination'))
+    sc_paramFormSet = modelformset_factory(control_parameter, fields=('control_parameter_id','value'), widgets={'value': Textarea(attrs={'cols':30,'rows':1})})
+    sc_statementFormSet = modelformset_factory(control_statement, fields=('control_statement_id','control_statement_text'))
+    if request.method == "POST":
+        sc_paramFormSet = sc_paramFormSet(queryset=control_parameter.objects.filter(system_control=sc.id))
+        if sc_paramFormSet.is_valid():
+            sc_paramFormSet.save(commit=False)
+            # paramater Titles are control title - paramater id
+            sc_paramFormSet.title = sc.title + ' - ' + sc_paramFormSet.control_parameter_id
+            # paramater short_name should be the control it relates to
+            sc_paramFormSet.short_name = sc.short_name
+            sc_paramFormSet.save()
+        else:
+            form_is_not_valid = True
+        if sc_statementFormSet.is_valid():
+            sc_statementFormSet.save(commit=False)
+            # Statement Titles are control title - paramater id
+            sc_statementFormSet.title = sc.title + ' - ' + sc_statementFormSet.control_statement_id
+            # Statement short_name should be the control it relates to
+            sc_statementFormSet.short_name = sc.short_name
+            sc_statementFormSet.save()
+        else:
+            form_is_not_valid = True
+        if sc_FormSet.is_valid():
+            sc_FormSet.save()
+        else:
+            form_is_not_valid = True
+        if not form_is_not_valid:
+            return redirect('ssp/system_control_edit.html', pk=pk)
+    else:
+        sc_formset = sc_FormSet(queryset=system_control.objects.filter(pk=sc.id))
+        param_formset = sc_paramFormSet(queryset=control_parameter.objects.filter(system_control=sc.id))
+        statement_formset = sc_statementFormSet(queryset=control_statement.objects.filter(system_control=sc.id))
+    return render(request, 'ssp/system_control_edit.html', {'object': sc,'sc_formset': sc_formset,'param_formset':param_formset,'statement_formset':statement_formset})
 
 class nist_control_list_view(generic.ListView):
     model = nist_control
