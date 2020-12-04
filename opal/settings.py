f97472608dc11c1b6a8d97e45da5b8339a91459b
Dan Chandler <dchandler@spc-linux-03.login.omb.gov>
2020-12-04 16:21:21 +0000

    Switched default db to SQLite for now

--- opal/settings.py
+++ opal/settings.py
@@ -78,26 +78,26 @@ WSGI_APPLICATION = 'opal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR + '/db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'OMB-OSCAL',
-        'USER': 's-sql-oscal',
-        'PASSWORD': '0LIssE|Ji?dGpqHVv@:4',
-        'HOST': '172.24.192.67',
-        'PORT': '',
-        'OPTIONS': {
-            'driver': 'ODBC Driver 13 for SQL Server',
-        },
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + '/db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'sql_server.pyodbc',
+#        'NAME': 'OMB-OSCAL',
+#        'USER': 's-sql-oscal',
+#        'PASSWORD': '0LIssE|Ji?dGpqHVv@:4',
+#        'HOST': '172.24.192.67',
+#        'PORT': '',
+#        'OPTIONS': {
+#            'driver': 'ODBC Driver 13 for SQL Server',
+#        },
+#    },
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
