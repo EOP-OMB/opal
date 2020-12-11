c1d8345f51632024c527220170562e797b1da8e8
samirari <samira.isber@tcg.com>
2020-12-11 12:49:49 -0500

    Added REST framework serializer for base_classes_and_fields, common and users modules.

--- scripts/test_GetModelJson.py
+++ scripts/test_GetModelJson.py
@@ -1,23 +1,12 @@
-from ssp.serializers import *
-from ssp.models import *
-from rest_framework.renderers import JSONRenderer
-import json
+import ssp.models
+from ssp.models.users import *
 
 
 def run():
-    #Using Django Core serializer
-    #print(nist_control_parameter().get_serializer_json)
-
     #Using Dictionary property
     #print(nist_control_parameter().get_dictionary_json)
 
-    #Creating JSON for person model all data
-    print("---- information_type model JSON ----")
-    queryset = information_type.objects.filter(pk=1)
-    serializer = information_type_serializer(queryset, many=True)
-    data = serializer.data
-    json_data = JSONRenderer().render(data)
-    json_object = json.loads(json_data)
-    json_str = json.dumps(json_object, indent=2)
-    print(json_str)
+    #Creating JSON for person model pk=1
+    print("---- Person model JSON ----")
+    print(person().get_serializer_json(1))
 
