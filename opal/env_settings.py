9bf3ddbe7f2034b5e5e1afb08f0e48e9da076e2d
dan <dan@cs4p.net>
2021-01-05 13:12:01 -0500

    moved settings to env_settings.py

---
+++ opal/env_settings.py
@@ -0,0 +1,7 @@
+env = {
+    "env" : "production",
+    "opal_secret_key" : "=&98a-%loivi0af$kqc*@-3+_^m_2sy(hm$vyv&u9^$1_-nbw7",
+    "debug" : "False",
+    "allowed_hosts" : ["ssp.omb.gov"],
+    "database" : "postgres"
+}
