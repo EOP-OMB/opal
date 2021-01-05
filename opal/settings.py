ced5823db2cebc78de90cd57876ad1bd80eec31c
dan <dan@cs4p.net>
2021-01-05 12:50:44 -0500

    added debug mode

--- opal/settings.py
+++ opal/settings.py
@@ -95,24 +95,24 @@ WSGI_APPLICATION = 'opal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if env == "production" or env == "debug":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'opal_prod',
-            'USER': 'opal',
-            'PASSWORD': 'DcpwXkn3_muYG7fNyxds',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'opal_prod',
+        'USER': 'opal',
+        'PASSWORD': 'DcpwXkn3_muYG7fNyxds',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR + '/db.sqlite3',
-        }
-    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR + '/db.sqlite3',
+#     }
+# }
 
 #Samira:
 REST_FRAMEWORK = {
