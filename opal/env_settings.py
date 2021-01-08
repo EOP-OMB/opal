d4493a80060b41a928ee55764f225f89a00b15c6
Chandler <daniel_e._chandler@omb.eop.gov>
2021-01-08 10:15:24 -0500

    Update env_settings.py

--- opal/env_settings.py
+++ opal/env_settings.py
@@ -3,6 +3,6 @@ env = {
     "opal_secret_key" : "=&98a-%loivi0af$kqc*@-3+_^m_2sy(hm$vyv&u9^$1_-nbw7",
     "debug" : "False",
     "allowed_hosts" : ["ssp.omb.gov"],
-    "database" : "postgres"
-
+    "database" : "postgres",
+    "db_password" : "DcpwXkn3_muYG7fNyxds"
 }
