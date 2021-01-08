c93cf7a96f82a48af5e502c6288456b66b501425
Chandler <daniel_e._chandler@omb.eop.gov>
2021-01-08 10:17:59 -0500

    Update opal/env_settings.py

--- opal/env_settings.py
+++ opal/env_settings.py
@@ -1,3 +1,9 @@
+"""
+This file contains the production settings for the opal application as deployed at OMB.  
+To run a local copy for development or other reasons, add this file to your .gitignore file
+and then modify the values as needed. 
+"""
+
 env = {
     "env" : "production",
     "opal_secret_key" : "=&98a-%loivi0af$kqc*@-3+_^m_2sy(hm$vyv&u9^$1_-nbw7",
