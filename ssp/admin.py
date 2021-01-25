471bde3558845d9d2f4932957b82ffeae2eee4f6
dan <dan@cs4p.net>
2021-01-25 11:44:44 -0500

    update admin.py to add filter for primary system for system controls

--- ssp/admin.py
+++ ssp/admin.py
@@ -9,7 +9,8 @@ from ssp import models
 @admin.register(models.system_security_plan)
 class systemSecurityPlanAdmin(admin.ModelAdmin):
     filter_horizontal = ['information_types', 'system_services', 'system_interconnections',
-                         'system_inventory_items', 'properties', 'links', 'leveraged_authorization', 'additional_selected_controls']
+                         'system_inventory_items', 'properties', 'links', 'leveraged_authorization',
+                         'additional_selected_controls']
     filter_vertical = ['controls', 'system_components']
 
     fieldsets = (
@@ -18,7 +19,7 @@ class systemSecurityPlanAdmin(admin.ModelAdmin):
         }),
         ('System', {
             'fields': (('published', 'date_authorized', 'system_status'), ('version', 'oscalVersion'),
-                       'control_baseline','controls','system_components')
+                       'control_baseline', 'controls', 'system_components')
         }),
         ('FIPS Level', {
             'fields': ('information_types', (
@@ -26,7 +27,7 @@ class systemSecurityPlanAdmin(admin.ModelAdmin):
                 'security_objective_availability'))
         }),
         ('Users', {
-            'fields': ['system_users',]
+            'fields': ['system_users', ]
         }),
         ('System Diagrams', {
             'fields': (('authorization_boundary_diagram',
@@ -46,7 +47,7 @@ class systemSecurityPlanAdmin(admin.ModelAdmin):
 @admin.register(models.control_statement)
 class control_statementAdmin(admin.ModelAdmin):
     filter_horizontal = ['control_statement_responsible_roles', 'properties', 'links', 'annotations']
-    #fields = ['title', 'short_name', 'control_statement_id', 'control_statement_responsible_roles','control_statement_text', 'links', 'desc']
+    # fields = ['title', 'short_name', 'control_statement_id', 'control_statement_responsible_roles','control_statement_text', 'links', 'desc']
     readonly_fields = ['nist_control_text', ]
 
     fieldsets = (
@@ -54,10 +55,12 @@ class control_statementAdmin(admin.ModelAdmin):
             'fields': ('nist_control_text',)
         }),
         ('Title', {
-            'fields': (('title', 'short_name'))
+            'fields': ('title', 'short_name')
         }),
         ('Statement', {
-            'fields': (('control_statement_id', 'control_statement_text'),'control_statement_responsible_roles', 'links','remarks')
+            'fields': (
+                ('control_statement_id', 'control_statement_text'), 'control_statement_responsible_roles', 'links',
+                'remarks')
         }),
         ('Other', {
             'classes': ('collapse',),
@@ -69,6 +72,7 @@ class control_statementAdmin(admin.ModelAdmin):
 @admin.register(models.control_parameter)
 class control_parameterAdmin(admin.ModelAdmin):
     fields = [('title', 'short_name'), ('control_parameter_id', 'value'), 'desc']
+
 
 class control_parameterInline(admin.TabularInline):
     model = models.system_control.control_parameters.through
@@ -97,26 +101,26 @@ class control_statementInline(admin.TabularInline):
 @admin.register(models.system_control)
 class system_controlAdmin(admin.ModelAdmin):
     filter_horizontal = ['properties', 'annotations', 'links', 'control_parameters', 'control_statements']
-    list_filter = ['control_origination', 'nist_control__group_title']
+    list_filter = ['control_primary_system','control_origination', 'nist_control__group_title']
     list_display = ['title', 'short_name', 'nist_control']
     sortable_by = ['sort_id', 'nist_control']
-    inlines = [control_parameterInline,control_statementInline]
-    readonly_fields = ['nist_control_text',]
+    inlines = [control_parameterInline, control_statementInline]
+    readonly_fields = ['nist_control_text', ]
 
     fieldsets = (
         ('NIST', {
             'fields': ('nist_control_text',)
         }),
         ('Title', {
-            'fields': (('title', 'short_name'))
+            'fields': ('title', 'short_name')
         }),
         ('System', {
             'fields': (
-                'control_status', 'control_origination', 'nist_control','control_primary_system')
+                'control_status', 'control_origination', 'nist_control', 'control_primary_system')
         }),
         ('Other', {
             'classes': ('collapse',),
-            'fields': ('remarks', 'links', 'properties', 'annotations','desc'),
+            'fields': ('remarks', 'links', 'properties', 'annotations', 'desc'),
         }),
     )
 
@@ -137,17 +141,19 @@ class nist_control_parameterAdmin(admin.TabularInline):
     model = models.nist_control_parameter
     extra = 0
 
+
 class nist_control_statementAdmin(admin.TabularInline):
     model = models.nist_control_statement
     extra = 0
 
+
 @admin.register(models.nist_control)
 class nist_controlAdmin(admin.ModelAdmin):
-    list_filter = ['catalog','group_title']
+    list_filter = ['catalog', 'group_title']
     list_display = ['catalog', 'group_title', 'control_id', 'label']
     list_display_links = ['catalog', 'group_title', 'control_id', 'label']
-    ordering = ('sort_id','catalog')
-    inlines = [nist_control_parameterAdmin,nist_control_statementAdmin,]
+    ordering = ('sort_id', 'catalog')
+    inlines = [nist_control_parameterAdmin, nist_control_statementAdmin, ]
 
 
 @admin.register(models.link)
@@ -161,13 +167,16 @@ class emailInline(admin.TabularInline):
     model = models.person.email_addresses.through
     extra = 1
 
+
 class phoneInline(admin.TabularInline):
     model = models.person.telephone_numbers.through
     extra = 1
 
+
 class locationInline(admin.TabularInline):
     model = models.person.locations.through
     extra = 1
+
 
 @admin.register(models.person)
 class personAdmin(admin.ModelAdmin):
@@ -175,7 +184,7 @@ class personAdmin(admin.ModelAdmin):
                          'annotations', 'links']
     list_display = ['name', ]
     list_filter = ['organizations', 'locations']
-    inlines = [emailInline,phoneInline,locationInline]
+    inlines = [emailInline, phoneInline, locationInline]
 
     fieldsets = (
         ('Title', {
@@ -183,7 +192,7 @@ class personAdmin(admin.ModelAdmin):
         }),
         ('Description', {
             'classes': ('collapse',),
-            'fields': ['desc',]
+            'fields': ['desc', ]
         }),
         ('ID', {
             'fields': (
@@ -194,7 +203,7 @@ class personAdmin(admin.ModelAdmin):
         #         'email_addresses', 'telephone_numbers', 'locations',)
         # }),
         ('Groups', {
-            'fields': ['organizations',]
+            'fields': ['organizations', ]
         }),
     )
 
