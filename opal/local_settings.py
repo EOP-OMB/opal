a9d33f81e49a0f8731fb783f4919b2c3e558f14d
root <root@spc-linux-03.login.omb.gov>
2021-02-01 16:15:51 +0000

    updated local_settings.py

--- opal/local_settings.py
+++ opal/local_settings.py
@@ -5,10 +5,10 @@ and then modify the values as needed.
 """
 
 env = {
-    "env" : "dev_dan",
+    "env" : "production",
     "opal_secret_key" : "=&98a-%loivi0af$kqc*@-3+_^m_2sy(hm$vyv&u9^$1_-nbw7",
-    "debug" : "True",
-    "allowed_hosts" : ["*"],
+    "debug" : "False",
+    "allowed_hosts" : ["ssp.omb.gov"],
     "database" : "postgres",
     "db_password" : "DcpwXkn3_muYG7fNyxds",
     "db_user" : "opal",
