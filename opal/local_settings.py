e64d9f2e1a67aef3ec7689230dc1f8568b794751
root <root@spc-linux-03.login.omb.gov>
2021-02-23 17:54:33 -0500

    resolve merge conflicts

--- opal/local_settings.py
+++ opal/local_settings.py
@@ -4,14 +4,14 @@ and then modify the values as needed.
 """
 
 env = {
-    "env" : "development",
+    "env" : "production",
     "opal_secret_key" : "=am5inf!4e36^9xwzt3r5$j#kv@g%9c@yya5xa-8&6v!1_bvq!",
-    "debug" : "True",
-    "allowed_hosts" : ["*"],
+    "debug" : "False",
+    "allowed_hosts" : ["ssp.omb.gov"],
     "database" : "postgres",
     #"database" : "sqlite",
-    "db_name" : "opal_dev",
-    "db_password" : "someting_good",
+    "db_name" : "opal_prod",
+    "db_password" : "DcpwXkn3_muYG7fNyxds",
     "db_user" : "opal_dev",
     "db_host" : "localhost",
     "db_port" : "",
