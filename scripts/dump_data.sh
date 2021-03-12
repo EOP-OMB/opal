4b547947740e7516c3de7cc2ef69feafb5b07809
root <root@spc-linux-03.login.omb.gov>
2021-03-12 14:15:49 -0500

    New data Dump

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py dumpdata ssp --indent 2 > "/opt/opal/ssp/fixtures/prod_data/$(date +"%Y_%m_%d_%I_%M_%p")_omb_data.log"
+python /opt/opal/manage.py dumpdata -a --indent 2 --format json --natural-foreign --natural-primary -o "/opt/opal/ssp/fixtures/prod_data/$(date +"%Y_%m_%d_%I_%M_%p")_omb_data.json" -v3
