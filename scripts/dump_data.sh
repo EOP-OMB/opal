e65cfe6394213cd0ca77c68d3edb27bf846b879d
root <root@spc-linux-03.login.omb.gov>
2021-03-12 11:09:00 -0500

    updated data_dump script

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -1,1 +1,2 @@
- python manage.py dumpdata ssp --indent 2 > ssp/fixtures/opal_ssp_db.json
+#!/bin/bash
+python manage.py dumpdata ssp --indent 2 > "/opt/opal/ssp/fixtures/prod_data/$(date +"%Y_%m_%d_%I_%M_%p")_omb_data.log"
