014c023f13600cebd779c1d28ef89e968d60a941
root <root@spc-linux-03.login.omb.gov>
2021-03-12 14:30:39 -0500

    New data Dump

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-python /opt/opal/manage.py dumpdata -a --indent 2 --format json --natural-foreign --natural-primary -o "/opt/opal/ssp/fixtures/prod_data/$(date +"%Y_%m_%d_%I_%M_%p")_omb_data.json" -v3
+
+# This uses natural forign keys, but that doesn't work
+#python /opt/opal/manage.py dumpdata -a --indent 2 --format json --natural-foreign --natural-primary -o "/opt/opal/ssp/fixtures/prod_data/$(date +"%Y_%m_%d_%I_%M_%p")_omb_data.json" -v3
+
+python /opt/opal/manage.py dumpdata -a --indent 2 --format json -o "/opt/opal/ssp/fixtures/prod_data/$(date +"%Y_%m_%d_%I_%M_%p")_omb_data.json" -v3
+
+git add "/opt/opal/ssp/fixtures/prod_data/$(date +"%Y_%m_%d_%I_%M_%p")_omb_data.json"
+git commit -m "New data Dump"
+git push
