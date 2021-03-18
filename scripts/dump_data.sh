caf16a1fc1f015beee722d0d76e356240b7b5281
root <root@spc-linux-03.login.omb.gov>
2021-03-18 12:27:42 -0400

    updated to data_dump script

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -10,11 +10,17 @@ python /opt/opal/manage.py dumpdata ssp.information_type -o /opt/opal/ssp/fixtur
 python /opt/opal/manage.py dumpdata ssp.user_function -o /opt/opal/ssp/fixtures/fixture_user_function.json --indent 2
 python /opt/opal/manage.py dumpdata ssp.user_privilege -o /opt/opal/ssp/fixtures/fixture_user_privilege.json --indent 2
 python /opt/opal/manage.py dumpdata ssp.user_role -o /opt/opal/ssp/fixtures/fixture_user_role.json --indent 2
-python /opt/opal/manage.py dumpdata ssp.port_range, ssp.protocol -o /opt/opal/ssp/fixtures/fixture_protocal_and_port.json --indent 2
-python /opt/opal/manage.py dumpdata ssp.inventory_item_type, ssp.protocol -o /opt/opal/ssp/fixtures/inventory_item_type.json --indent 2
+python /opt/opal/manage.py dumpdata ssp.port_range ssp.protocol -o /opt/opal/ssp/fixtures/fixture_protocal_and_port.json --indent 2
+python /opt/opal/manage.py dumpdata ssp.inventory_item_type -o /opt/opal/ssp/fixtures/inventory_item_type.json --indent 2
 
 python /opt/opal/manage.py dumpdata -a --indent 2 --format json -o "/opt/opal/ssp/fixtures/prod_data/$(date +"%Y_%m_%d_%I_%M_%p")_omb_data.json" -v3
 
 git add "/opt/opal/ssp/fixtures/prod_data/$(date +"%Y_%m_%d_%I_%M_%p")_omb_data.json"
-git commit -m "New data Dump"
+git add fixture_status.json
+git add fixture_user_privilege.json
+git add fixture_information_type.json
+git add fixture_user_function.json
+git add fixture_user_role.json
+
+git commit -m "New data dump"
 git push
