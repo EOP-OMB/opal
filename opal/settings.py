4c5d03d134a8de9b146141b15b6f3fee369208bb
dan <dan@cs4p.net>
2022-03-24 14:08:08 -0400

    fix for allowed_hosts

--- opal/settings.py
+++ opal/settings.py
@@ -49,8 +49,8 @@ SECRET_KEY = env("OPAL_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
-# ALLOWED_HOSTS = env("ALLOWED_HOSTS")
-ALLOWED_HOSTS = ("localhost", "127.0.0.1",)
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(',')
+
 
 # Application definition
 
