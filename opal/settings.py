f33d7817e9731419f07bddf0eeebab1e8dbf5892
dan <dan@cs4p.net>
2022-03-24 13:07:03 -0400

    update settings.py

--- opal/settings.py
+++ opal/settings.py
@@ -19,7 +19,7 @@ import environ
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
