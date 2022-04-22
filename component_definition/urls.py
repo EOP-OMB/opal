b95d9219160b8e1c9a3a33f1489e04f21ffe1fb0
dan <dan@cs4p.net>
2022-04-22 08:19:35 -0400

    Merge remote-tracking branch 'origin/opal_v2' into opal_v2

--- component_definition/urls.py
+++ component_definition/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('<int:pk>', component_detail_view.as_view(), name='component_detail_view'),
     path('new', create_component_view.as_view(), name='create_component_view'),
     path('parameter/new/<int:param_id>', create_parameter_view.as_view(), name='create_parameter_view'),
-    path('implemented_requirements/new', create_implemented_requirements_view.as_view(),name='create_implemented_requirements_view'),
-    path('requirement/new/<int:control_id>', implemented_requirements_form_view, name='new_requirement'),
+    # path('implemented_requirements/new', create_implemented_requirements_view.as_view(),name='create_implemented_requirements_view'),
+    # path('requirement/new/<int:control_id>', implemented_requirements_form_view, name='new_requirement'),
     path('requirement/define/', create_component_statement, name='create_component_statement')
     ]
