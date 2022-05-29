498aac072540dba0fb27209c1923ab946cff91bd
dan <dan@cs4p.net>
2022-05-29 13:33:18 -0400

    remove test_settings.py

--- opal/test_settings.py
+++
@@ -1,8 +0,0 @@
-from opal.settings import *
-
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, DB_NAME),
-            }
-        }
-
