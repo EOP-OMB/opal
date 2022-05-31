c607bad485acdaa654b6c0032f008ae1b23e5ee0
dan <dan@cs4p.net>
2022-05-31 09:53:14 -0400

    added saml code into project

---
+++ sp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.metadata, name="sp-idp-metadata"),
+    path("acs/", views.acs, name="sp-idp-acs"),
+    path("slo/", views.slo, name="sp-idp-slo"),
+    path("login/", views.login, name="sp-idp-login"),
+    path("test/", views.login, {"test": True}, name="sp-idp-test"),
+    path("verify/", views.login, {"verify": True}, name="sp-idp-verify"),
+    path("logout/", views.logout, name="sp-idp-logout"),
+]
