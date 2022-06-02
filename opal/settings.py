b81365b2297013a02355cbcdf8dbddd41032cb32
dan <dan@cs4p.net>
2022-06-02 12:07:04 -0400

    fix for static files

--- opal/settings.py
+++ opal/settings.py
@@ -20,9 +20,9 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'vendor')]
 
--- opal/urls.py
+++ opal/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.generic.base import TemplateView
 
 from common.views import index_view
 
