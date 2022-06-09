4b208f035e906124714cc81d09dc964a95ee5b05
dan <dan@cs4p.net>
2022-06-09 12:07:01 -0400

    fix startup script to allow sattic files hosted by mod_wsgi-express

--- startup.sh
+++ startup.sh
@@ -4,7 +4,8 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-mod_wsgi-express start-server opal/wsgi.py
+#mod_wsgi-express start-server opal/wsgi.py
+mod_wsgi-express start-server --url-alias /static static --application-type module opal/wsgi.py
 
 
 
