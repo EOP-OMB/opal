e7bc8114e977826d7569fb51d22cabfcac6203d2
dan-omb <dchandler@omb.eop.gov>
2022-07-19 14:16:46 -0400

    adding migration files

--- catalog/migrations/0002_available_catalog_list_catalog_uuid_and_more.py
+++ catalog/migrations/0002_available_catalog_list_catalog_uuid_and_more.py
@@ -2,6 +2,7 @@
 
 from django.db import migrations, models
 import uuid
+import common.models
 
 
 class Migration(migrations.Migration):
@@ -11,6 +12,23 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='available_catalog_list',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created_at', models.DateTimeField(auto_now_add=True, null=True)),
+                ('updated_at', models.DateTimeField(auto_now=True, null=True)),
+                ('remarks', models.TextField(blank=True, default='', help_text='Additional commentary on the containing object.', verbose_name='Remarks')),
+                ('uuid', models.UUIDField(default=uuid.uuid4, unique=True)),
+                ('slug', common.models.ShortTextField(help_text='A short name used to identify the catalog in functions or queries. Lowercase, no spaces.', verbose_name='Catalog Slug')),
+                ('link', models.URLField(help_text='A complete URL which returns valid OSCAL json text', verbose_name='Link to Catalog')),
+                ('name', common.models.ShortTextField(help_text='Human readable name of the catalog.', verbose_name='Catalog Title')),
+                ],
+            options={
+                'verbose_name': 'Catalog Source',
+                'verbose_name_plural': 'Catalog Sources',
+                },
+            ),
         migrations.AddField(
             model_name='available_catalog_list',
             name='catalog_uuid',
