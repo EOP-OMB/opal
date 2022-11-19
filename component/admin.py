0a94f8a180b8d1fd8f59e891fdf6b62710f0ff8d
dan-omb <dchandler@omb.eop.gov>
2022-11-19 10:43:55 -0500

    removed nested_inline admin views

--- component/admin.py
+++ component/admin.py
@@ -1,6 +1,6 @@
 from django.apps import apps
 from django.contrib import admin
-from nested_inline.admin import NestedModelAdmin, NestedStackedInline
+# from nested_inline.admin import NestedModelAdmin, NestedStackedInline
 
 from common.admin import CustomAdmin
 from component.models import by_components, components, control_implementations, implemented_requirements
@@ -15,44 +15,44 @@ from component.models import by_components, components, control_implementations,
 #     filter_horizontal = ('statement_id',)
 
 
-class by_componentTabularInline(NestedStackedInline):
-    model = by_components
-    extra = 0
-    fk_name = 'implemented_requirement'
-    fields = ('component_uuid', 'implementation_status', 'description', 'responsible_roles')
-    filter_horizontal = ('responsible_roles',)
-
-
-class implemented_requirementsTabularInline(NestedStackedInline):
-    model = implemented_requirements
-    extra = 0
-    fk_name = 'control_implementation'
-    inlines = [by_componentTabularInline]
-    fields = ('control_id',)
-
-
-class control_implementationsTabularInline(NestedStackedInline):
-    model = control_implementations
-    extra = 0
-    fk_name = 'component'
-    fields = ('description',)
-    inlines = [implemented_requirementsTabularInline]
-
-
-@admin.register(components)
-class componentsAdmin(NestedModelAdmin):
-    list_display = (
-        'title',
-        'type',
-        'status',
-        'created_at',
-        'updated_at',
-        )
-    list_filter = ('created_at', 'updated_at', 'status', 'type')
-    raw_id_fields = ('props', 'links', 'responsible_roles', 'protocols')
-    date_hierarchy = 'created_at'
-    inlines = [control_implementationsTabularInline]
-    fields = ('title', 'type', 'description', 'purpose', 'status')
+# class by_componentTabularInline(NestedStackedInline):
+#     model = by_components
+#     extra = 1
+#     fk_name = 'implemented_requirement'
+#     fields = ('component_uuid', 'implementation_status', 'description', 'responsible_roles')
+#     filter_horizontal = ('responsible_roles',)
+#
+#
+# class implemented_requirementsTabularInline(NestedStackedInline):
+#     model = implemented_requirements
+#     extra = 1
+#     fk_name = 'control_implementation'
+#     inlines = [by_componentTabularInline]
+#     fields = ('control_id',)
+#
+#
+# class control_implementationsTabularInline(NestedStackedInline):
+#     model = control_implementations
+#     extra = 1
+#     fk_name = 'component'
+#     fields = ('description',)
+#     inlines = [implemented_requirementsTabularInline]
+#
+#
+# @admin.register(components)
+# class componentsAdmin(NestedModelAdmin):
+#     list_display = (
+#         'title',
+#         'type',
+#         'status',
+#         'created_at',
+#         'updated_at',
+#         )
+#     list_filter = ('created_at', 'updated_at', 'status', 'type')
+#     raw_id_fields = ('props', 'links', 'responsible_roles', 'protocols')
+#     date_hierarchy = 'created_at'
+#     inlines = [control_implementationsTabularInline]
+#     fields = ('title', 'type', 'description', 'purpose', 'status')
 
 
 # other models
