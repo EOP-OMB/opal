98a4be0e6704125c5a36750866172b7cce36267a
dan-omb <dchandler@omb.eop.gov>
2023-02-02 17:35:35 -0500

    added a hardcoded SP_PREPARE_REQUEST environment variable

--- opal/settings.py
+++ opal/settings.py
@@ -24,6 +24,8 @@ from logging.handlers import RotatingFileHandler
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
@@ -77,6 +79,7 @@ SAML_SCRIPT_NAME = os.getenv("SAML_SCRIPT_NAME", default=False)  # should be the
 SAML_SERVER_PORT = os.getenv("SAML_SERVER_PORT", default=False)
 # SAML_PROVIDERS must be a comma seperated list of idp stubs that will be used in the application
 SAML_PROVIDERS = os.getenv("SAML_PROVIDERS", default="stub")
+SP_PREPARE_REQUEST = "common.auth_functions.prepare_request"
 # Handling allowed hosts a little different since we have to turn it into a list.
 # If providing a value, you just need to provide a comma separated string of hosts
 # You don't need to quote anything or add [] yourself.
