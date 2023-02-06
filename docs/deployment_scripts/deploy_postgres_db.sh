eee85df103b8d4bc85379572f5c91fe10e905192
dan-omb <dchandler@omb.eop.gov>
2023-02-06 16:33:31 -0500

    commented out the docker volume delete step

--- docs/deployment_scripts/deploy_postgres_db.sh
+++ docs/deployment_scripts/deploy_postgres_db.sh
@@ -3,8 +3,8 @@
 docker stop $(docker ps | grep opaldb | cut -f1 -d " ")
 docker rm $(docker ps -a | grep opaldb | cut -f1 -d " ")
 
-docker volume rm postgresql_data
+#docker volume rm postgresql_data
+#docker volume create postgresql_data
 
 docker build -t opaldb ../postgres/.
-docker volume create postgresql_data
 docker run -it -d -p 5432:5432 --name opaldb  -v postgresql_data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=use_a_secure_password_here -e POSTGRES_OPAL_PASSWORD=password opaldb
