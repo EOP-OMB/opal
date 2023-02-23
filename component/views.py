8cdec2237444d7ca13b68171ac6b536fd6bee360
dan-omb <dchandler@omb.eop.gov>
2023-02-23 16:01:20 -0500

    fixed bug in component list view

--- component/views.py
+++ component/views.py
@@ -25,8 +25,8 @@ def component_list_view(request):
         ctrl_list = comp.list_implemented_controls()
         html_ctrl_list = []
         for ctrl in ctrl_list:
-            html_ctrl_list.append("<a href=%s target=_blank>%s</a>" % (ctrl.get_absolute_url, ctrl.__str__))
-        html_str += "<tr><td>%s<td><td>%s<td><td>%s<td>" % (comp.title, comp.status, ', '.join(ctrl_list))
+            html_ctrl_list.append("<a href='%s' target='_blank'>%s</a>" % (ctrl.get_absolute_url(), ctrl.control_id))
+        html_str += "<tr><td>%s<td><td>%s<td><td>%s<td>" % (comp.title, comp.status, ', '.join(html_ctrl_list))
     context = {
         'content': html_str,
         'title': 'Component List'
@@ -40,20 +40,20 @@ class component_detail_view(DetailView):
     template_name = "generic_detail.html"
 
 
-# def component_workflow_view():
-#     html_str = "<h1>Steps to create a Component</h1>"
-#     html_str += "<ol><li>Create the component</li>"
-#     html_str += "<li>Create a control_implementations</li>"
-#     html_str += "<li>Add the controls</li></ol>"
-#     return html_str
-#
-#
-# def components_form_view(request):
-#     context = {}
-#     form = components_form(request.POST or None, request.FILES or None)
-#
-#     if form.is_valid():
-#         form.save()
-#
-#     context['form'] = form
-#     return render(request, "generic_form.html", context)
+def component_workflow_view():
+    html_str = "<h1>Steps to create a Component</h1>"
+    html_str += "<ol><li>Create the component</li>"
+    html_str += "<li>Create a control_implementations</li>"
+    html_str += "<li>Add the controls</li></ol>"
+    return html_str
+
+
+def components_form_view(request):
+    context = {}
+    form = components_form(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+
+    context['form'] = form
+    return render(request, "generic_form.html", context)
