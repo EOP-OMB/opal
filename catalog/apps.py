dcfe0f05742bfdd756d23e173815a9410ded928e
dan-omb <dchandler@omb.eop.gov>
2023-04-18 14:38:42 -0400

    add function to create sort_id on controls when the application starts

--- catalog/apps.py
+++ catalog/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'catalog'
+    
+    def ready(self):
+        from catalog.models import controls
+        for c in controls.objects.filter(sort_id=None).all():
+            c.set_sort_id()
+            c.save()
