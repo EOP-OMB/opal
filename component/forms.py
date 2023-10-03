29be410d978a80e4d179d763f16d4e488ac1ffbc
dan-omb <dchandler@omb.eop.gov>
2023-10-03 13:28:44 -0400

    updated component forms

--- component/forms.py
+++ component/forms.py
@@ -9,10 +9,9 @@ class ComponentForm(forms.ModelForm):
     
     class Meta:
         model = components
-        fields = ['type', 'title', 'purpose', 'status', 'props', 'description', 'links']
-        widgets = {'props': RelatedFieldWidgetCanAdd(props, related_url='common:add_props_view'),
-                   'links': RelatedFieldWidgetCanAdd(links, related_url='common:add_links_view')
-                   }
+        # fields = ['type', 'title', 'purpose', 'status', 'props', 'description', 'links']
+        fields = ['type', 'title', 'purpose', 'status', 'description']
+        # widgets = {'props': RelatedFieldWidgetCanAdd(props, related_url='common:add_props_view'),'links': RelatedFieldWidgetCanAdd(links, related_url='common:add_links_view')                   }
         
 
     def __init__(self, *args, **kwargs):
@@ -21,4 +20,7 @@ class ComponentForm(forms.ModelForm):
         self.fields['purpose'].widget.attrs.update(style="width: inherit;")
         self.fields['status'].widget.attrs.update(style="width: inherit;")
         self.fields['type'].widget.attrs.update(style="width: inherit;")
+        self.fields['description'].label = ("Description")
+        # self.fields['props'].widget.attrs.update(style="width: inherit;")
+        # self.fields['links'].widget.attrs.update(style="width: inherit;")
 
