4e9e78fc24d0e90bbccaba0c4bdf7ccc5589639d
dan-omb <dchandler@omb.eop.gov>
2023-10-20 14:40:09 -0400

    changed default db name

---
+++ docs/postgres/backup_and_restore.sh
@@ -0,0 +1,13 @@
+DUMP
+// pod-name         name of the postgres pod
+// postgres-user    database user that is able to access the database
+// database-name    name of the database
+kubectl -n opal exec $(kubectl get pods -n opal | grep opal-postgres-db | cut -f 1 -d ' ' -s) -- bash -c "pg_dump -U postgres opal" > database.sql
+
+
+
+RESTORE
+// pod-name         name of the postgres pod
+// postgres-user    database user that is able to access the database
+// database-name    name of the database
+cat database.sql | kubectl exec -i [pod-name] -- psql -U [postgres-user] -d [database-name]
