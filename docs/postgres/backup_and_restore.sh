167e441f7001a0e6934c103bf7a0d35c22665095
dan-omb <dchandler@omb.eop.gov>
2024-02-26 11:52:10 -0500

    updated backup_and_restore.sh script

--- docs/postgres/backup_and_restore.sh
+++ docs/postgres/backup_and_restore.sh
@@ -10,4 +10,4 @@ RESTORE
 // pod-name         name of the postgres pod
 // postgres-user    database user that is able to access the database
 // database-name    name of the database
-cat database.sql | kubectl exec -i [pod-name] -- psql -U [postgres-user] -d [database-name]
+cat database.sql | kubectl exec -i $(kubectl get pods -n opal | grep opal-postgres-db | cut -f 1 -d ' ' -s) -- psql -U postgres -d opal
