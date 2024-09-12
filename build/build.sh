e330244e2f05d0eceffbe86f9f1c8acf041b6aa7
dan-omb <dchandler@omb.eop.gov>
2024-09-12 14:09:58 -0400

    Refactor Helm chart and update deployment scripts
    Deleted obsolete Helm chart templates and configuration files to streamline deployment. Renamed and relocated remaining template files for better organization. Updated build script to use Helm for package building and deployment to improve efficiency.

--- build/build.sh
+++ build/build.sh
@@ -8,6 +8,7 @@ docker push 192.168.42.100:5050/opal-webapp
 docker build -t 192.168.42.100:5050/opal-db -f Dockerfile_db .
 docker push 192.168.42.100:5050/opal-db
 
-kubectl apply -f yaml/app/
-kubectl apply -f yaml/db/
-kubectl apply -f yaml/nginx/
+helm package ./opal/ --app-version $(cat ../version.txt)
+
+#helm install opal opal-$(cat opal/Chart.yaml| grep version: | cut -d' ' -f2).tgz --atomic --create-namespace --debug
+helm template opal opal-$(cat opal/Chart.yaml| grep version: | cut -d' ' -f2).tgz > helm_output.yaml
